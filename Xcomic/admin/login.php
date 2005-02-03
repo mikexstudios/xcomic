4d82516b97aac6926733ab4000e860a98df43b9a
Michael Huynh <mike.huynh@gmail.com>
2005-02-03 02:35:04 +0000

    Fix for Firefox. It seems that the cookie md5 password is injecting itself inside the form's password field. Changed the name of the form password field to fix this.

--- Xcomic/admin/login.php
+++ Xcomic/admin/login.php
@@ -14,14 +14,17 @@ require_once $xcomicRootPath.'initialize.php';	//Include all page common setting
 include_once $xcomicRootPath.'includes/Security.'.$classEx;
 include_once './classes/UserManagement.'.$classEx; //Login/Logout
 
+//Form field variables
+$formUsername = 'loginUsername';
+$formPassword = 'loginPassword';
 
 //Create objects
 $security = new Security();
 $userManagement = new UserManagement;
 
 //Get input from form
-$inUsername = (!empty($_REQUEST['username'])) ? $security->allowOnlyChars($_REQUEST['username']) : null;
-$inPassword = (!empty($_REQUEST['password'])) ? $security->allowOnlyChars($_REQUEST['password']) : null;
+$inUsername=(!empty($_REQUEST[$formUsername])) ? $security->allowOnlyChars($_REQUEST[$formUsername]) : NULL;
+$inPassword=(!empty($_REQUEST[$formPassword])) ? $security->allowOnlyChars($_REQUEST[$formPassword]) : NULL;
 	
 //Set them in User Management
 $userManagement->setUsername($inUsername);
@@ -43,10 +46,10 @@ include './includes/header.php';
  <div class="section-body">
   <form method="POST" action="" enctype="multipart/form-data">
    <label for="username">Username:</label><br />
-   <input type="text" name="<?php echo 'username'; ?>" size="20" /><br />
+   <input type="text" name="<?php echo $formUsername; ?>" size="20" /><br />
 
    <label for="password">Password:</label><br />
-   <input type="password" name="<?php echo 'password'; ?>" size="20" /><br />
+   <input type="password" name="<?php echo $formPassword; ?>" size="20" /><br />
 
    <input type="submit" name="submit" value="Login" />
   </form>
