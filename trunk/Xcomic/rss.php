216dc8ada022414b286b9e2c6f330902337aab38
squeeself <squeeself@d57e50ae-ce0d-0410-99f9-be9e1b618dd5>
2005-12-19 23:44:02 +0000

    Reverted to the pre-0.9.0 version of rss.php (revision 1.2). Then, fixed it all up to work with 0.9.0 and fixed a couple of just general errors and php notices.

--- trunk/Xcomic/rss.php
+++ trunk/Xcomic/rss.php
@@ -1,75 +1,148 @@
 <?php
-function rfc822($ctime) {
-	//Makes RFC822-formatted time/date string
-	//Required to follow RSS 2.0 specification
-	$date = date('D, d M Y H:i:s', $ctime);
-	$tz = date('T', $ctime);
-	$tz = explode(' ', $tz); $i = 0;
-	$shortz = '';
-	while ($i < sizeOf($tz)) {
-		$shortz .= substr($tz[$i], '0', '1');
-		$i++;
-	}
-	$date .= " $shortz";
-	return $date;
-}
-
 //Do necessary things
 $xcomicRootPath = './';
-require_once $xcomicRootPath.'initialize.php';	//Include all page common settings
+require_once $xcomicRootPath.'initialize.php';
 include $xcomicRootPath.'Xcomic.php';
 $xcomic = new Xcomic($db);
+
+//Include RSSCreator
+include $xcomicRootPath.'includes/RSS.class.php';
+$rss = new XML_RSSCreator('RSS 2.0');
 
 //Check if RSS is enabled
 $rssEnabled = $settings->getSetting('enableRSS');
 if (!$rssEnabled) {
 	die;
 }
-else {
+elseif ($rssEnabled) {
 	//For execution time
 	$start = microtime();
 
-	//Information for <channel>
-	$comicTitle = stripslashes($settings->getSetting('title'));
-	$comicLink = 'http://'.$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['PHP_SELF']), '/\\') . '/';
-	$comicTotal = $settings->getSetting('rssNumComics');
-	//$comicID = $xcomic->comicDisplay->getLatestComicID();
-	
-	//Array of the comics to be syndicated
-	$comics = $xcomic->dbc->getAll('SELECT * FROM '.XCOMIC_COMICS_TABLE.' ORDER BY cid DESC LIMIT '.$comicTotal);
-	
-	//There were no comics retrieved for some reason (including none posted in the first place)
-	if (sizeOf($comics) == '0') { die; }
-
-	$comicDate = $xcomic->comic->getDate();
-
-	header('Content-type: text/xml');
-	echo '<?xml version="1.0" encoding="UTF-8"?>
-		<rss version="2.0">
-			<channel>
-				<title>'.$comicTitle.'</title>
-				<link>'.$comicLink.'</link>
-				<description>'.$comicTitle.' RSS Feed</description>
-				<pubDate>'.rfc822($comicDate).'</pubDate>
-				<lastBuildDate>'.rfc822(time()).'</lastBuildDate>
-				<docs>http://blogs.law.harvard.edu/tech/rss</docs>';
-	$i = 0;
-	while ($i < sizeOf($comics)) {
-		$title = stripslashes($comics[$i]['title']);
-		$link = $comicLink . $comics[$i]['filename'];
-		$guid = $comicLink . 'index.php?cid=' . $comics[$i]['cid'];
-		echo '
-			<item>
-				<title>'.$title.'</title>
-				<link>'.$link.'</link>
-				<description>'.$title.'</description>
-				<pubDate>'.rfc822($comics[$i]['date']).'</pubDate>
-				<guid>'.$guid.'</guid>
-			</item>';
-		$i++;
+	//Check what $_GET['feed'] is; if null, assume 'both', and if 'news' or 'comics', get just the news/comics
+	$feed = isset($_GET['feed']) ? $_GET['feed'] : null;
+	if ((!$feed) || (($feed != 'news') && ($feed != 'comics') && ($feed != 'both'))) {
+		$feed = 'both';
 	}
-	$finish = microtime();
-	$total = $finish - $start;
-	echo '<generator>Xcomic, http://xcomic.sourceforge.net, generated in '.$total.'s</generator></channel></rss>';
+
+	$rss->title = $xcomic->security->removeMagicQuotes($settings->getSetting('title'));
+	$rss->link = 'http://'.$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['PHP_SELF']), '/\\') . '/';
+	//$rss->desc = $xcomic->security->removeMagicQuotes($settings->getSetting('title'));
+	$rss->desc = $rss->title . ', powered by Xcomic';
+
+	//Only fetch this many rows, since we'll only return this many to the user anyway
+	$maxSyndicate = $settings->getSetting('rssNumComics');
+
+	switch ($feed) {
+		case 'news':
+			$data = $xcomic->dbc->getAll('SELECT * FROM '.XCOMIC_NEWS_TABLE.' ORDER BY id DESC LIMIT '.$maxSyndicate);
+			//No news to syndicate, or an error
+			if (sizeOf($data) == '0') {
+				die;
+			}
+
+			$rss->pubDate = $rss->rfc822Date($xcomic->news->getDate());
+			$rss->buildDate = $rss->rfc822Date(time());
+			$rss->docs = 'http://blogs.law.harvard.edu/tech/rss';
+
+			$newsItems = sizeOf($data);
+			$i = 0;
+			while ($i < $newsItems) {
+				$item = array(
+					'title' => $data[$i]['title'],
+					'description' => $data[$i]['username'] . ' writes, "' . $data[$i]['content'] . '"',
+					'pubDate' => $rss->rfc822date($data[$i]['date'])
+				);
+				$rss->addItem($item);
+				$i++;
+			}
+
+			//Now to echo it
+			$finish = microtime();
+			$rss->generator = 'Xcomic, http://xcomic.sourceforge.net, generated in ' . ($finish - $start) . 's';
+			$rssData = $rss->save();
+			header('Content-type: text/xml');
+			echo $rssData;
+		break;
+
+		case 'comics':
+			$data = $xcomic->dbc->getAll('SELECT * FROM '.XCOMIC_COMICS_TABLE.' ORDER BY cid DESC LIMIT '.$maxSyndicate);
+			//No comics to syndicate, or an error
+
+			if (sizeOf($data) == '0') {
+				die;
+			}
+
+			$rss->pubDate = $rss->rfc822Date($xcomic->comic->getDate());
+			$rss->buildDate = $rss->rfc822Date(time());
+			$rss->docs = 'http://blogs.law.harvard.edu/tech/rss';
+
+			$comicItems = sizeOf($data);
+			$i = 0;
+			while ($i < $comicItems) {
+				$item = array(
+					'title' => $data[$i]['title'],
+					'description' => '<img src="' . $rss->link . 'comics/' . $data[$i]['filename'] . '" alt="' . $data[$i]['title'] . '" />',
+                    'pubDate' => $rss->rfc822date($data[$i]['date']),
+					'guid' => $rss->link . '/index.php?cid=' . $data[$i]['cid']
+				);
+				$rss->addItem($item);
+				$i++;
+			}
+
+			//Now to echo it
+			$finish = microtime();
+			$rss->generator = 'Xcomic, http://xcomic.sourceforge.net, generated in ' . ($finish - $start) . 's';
+			$rssData = $rss->save();
+			header('Content-type: text/xml');
+			echo $rssData;
+		break;
+
+		case 'both':
+
+		default:
+		$data = $xcomic->dbc->getAll('SELECT * FROM '.XCOMIC_NEWS_TABLE.' ORDER BY id DESC LIMIT '.$maxSyndicate);
+			//No news to syndicate, or an error
+			if (sizeOf($data) == '0') {
+				die;
+			}
+
+			$rss->pubDate = $rss->rfc822Date($xcomic->news->getDate());
+			$rss->buildDate = $rss->rfc822Date(time());
+			$rss->docs = 'http://blogs.law.harvard.edu/tech/rss';
+
+			$newsItems = sizeOf($data);
+			$i = 0;
+			while ($i < $newsItems) {
+				$item = array(
+					'title' => $data[$i]['title'],
+					'description' => $data[$i]['username'] . ' writes, "' . $data[$i]['content'] . '"',
+					'pubDate' => $rss->rfc822date($data[$i]['date'])
+				);
+				$rss->addItem($item);
+				$i++;
+			}
+
+			$comicdata = $xcomic->dbc->getAll('SELECT * FROM '.XCOMIC_COMICS_TABLE.' ORDER BY cid DESC LIMIT '.$maxSyndicate);
+			$comicItems = sizeOf($data);
+			$i = 0;
+			while ($i < $comicItems) {
+				$item = array(
+					'title' => $comicdata[$i]['title'],
+					'description' => '<img src="' . $rss->link . 'comics/' . $comicdata[$i]['filename'] . '" alt="' . $comicdata[$i]['title'] . '" />',
+					'pubDate' => $rss->rfc822date($comicdata[$i]['date']),
+					'guid' => $rss->link . '/index.php?cid=' . $comicdata[$i]['cid']
+				);
+				$rss->addItem($item);
+				$i++;
+			}
+
+			//Now to echo it
+			$finish = microtime();
+			$rss->generator = 'Xcomic, http://xcomic.sourceforge.net, generated in ' . ($finish - $start) . 's';
+			$rssData = $rss->save();
+			header('Content-type: text/xml');
+			echo $rssData;
+		break;
+	}
 }
 ?>
