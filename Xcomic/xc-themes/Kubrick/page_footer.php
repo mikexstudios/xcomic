23eb0f78f2537c8cea7010593888673a3e09d5e0
Michael Huynh <mike.huynh@gmail.com>
2006-01-13 07:34:14 +0000

    Second attempt at removing KUBRICK

--- Xcomic/xc-themes/Kubrick/page_footer.php
+++
@@ -1,9 +0,0 @@
-   <div id="footer">
-    <p>
-     <a href="admin/">Administration Panel</a>
-     <br />
-     Page generated in <?php out('getExecutionTime'); ?> seconds
-     <cite>Powered by <a href="http://xcomic.sourceforge.net" title="Powered by Xcomic, state-of-the-art web comic publishing platform"><strong>Xcomic</strong></a></cite> &copy; 2004-2005 Xcomic.<br />
-     <a href="http://binarybonsai.com/kubrick/">Kubrick Design by Michael Heilemann</a>.
-    </p>
-   </div>
