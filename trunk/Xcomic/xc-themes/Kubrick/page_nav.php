968d4bbd7fff7377db67a34c13a5b4b6ffa66efc
mikexstudios <mikexstudios@d57e50ae-ce0d-0410-99f9-be9e1b618dd5>
2006-01-13 07:40:19 +0000

    Added Kubrick...hopefully this time works...

---
+++ trunk/Xcomic/xc-themes/Kubrick/page_nav.php
@@ -0,0 +1,11 @@
+   <div id="navigation" class="middlecolumn">
+    <ul id="nav-menu">
+     <li><a href="<?php out('pageUrl', 'characters'); ?>">Characters</a></li>
+     <li><a href="<?php out('pageUrl', 'info'); ?>">Information</a></li>
+     <li><a href="">Other Works</a></li>
+     <li><a href="">Forum</a></li>
+     <li><a href="<?php out('pageUrl', 'links'); ?>">Links</a></li>
+     <li><a href="<?php out('pageUrl', 'about'); ?>">About</a></li>
+     <li><a href="">Blog</a></li>
+    </ul>
+   </div>
